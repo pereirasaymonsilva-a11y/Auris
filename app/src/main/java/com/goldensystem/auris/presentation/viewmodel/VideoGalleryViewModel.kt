e2a74a95af17a4b06ac65753318d7c63835ea950
pereirasaymonsilva <pereirasaymonsilva@gmail.com>
2026-05-15 11:30:19 -0300

    TELA PLAYER ATUALIZADA import

--- app/src/main/java/com/goldensystem/auris/presentation/viewmodel/VideoGalleryViewModel.kt
+++ app/src/main/java/com/goldensystem/auris/presentation/viewmodel/VideoGalleryViewModel.kt
@@ -13,6 +13,7 @@ import com.goldensystem.auris.utils.VideoUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
+import android.content.ContentUris
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
