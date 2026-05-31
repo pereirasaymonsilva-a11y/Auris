6b33af6595e09e3e7867b139e720b2d942d4c12d
pereirasaymonsilva <pereirasaymonsilva@gmail.com>
2026-05-31 19:28:03 -0300

    Auris#262

--- app/src/main/java/com/goldensystem/auris/data/local/AppDatabase.kt
+++ app/src/main/java/com/goldensystem/auris/data/local/AppDatabase.kt
@@ -22,7 +22,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "auris_database"
+                    "auris_viewcount"
                 ).fallbackToDestructiveMigration()
                  .build()
                 INSTANCE = instance
